30479d3f9a2837ad6e1ea7438caf0b31839d4ff6
Jorge Amador <rugama01@hotmail.com>
2019-08-18 16:47:22 -0600

    sda

--- js/Pojecto Clase 5 js.js
+++ js/Pojecto Clase 5 js.js
@@ -2,7 +2,7 @@ let nombre = "El Mero Mero ElmerOmero";
 console.log(nombre);
 const numero = 8.14;
 
-// let d = document.getElementById("unico");
+//let d = document.getElementById("unico");
 let d = document.getElementsByClassName("clase");
 console.log(d);
 console.log(d[0]);
@@ -10,9 +10,28 @@ console.log(d.length);
 
 let date = new Date();
 
-document.body.innerHTML = `<h2>dehumo</h2>`;
-document.body.innerHTML = `<h2>${date.getHours()}</h2>`;
-document.body.innerHTML = `<h2>${date.getHours()}</h2> <h1>${date.getMinutes()}</h1> <h2>${date.getSeconds()}</h2>`;
-
-
+//document.body.innerHTML = `<h2>dehumo</h2>`;
+//document.body.innerHTML = `<h2>${date.getHours()}</h2>`;
+//document.body.innerHTML = `<h2>${date.getHours()}</h2> <h1>${date.getMinutes()}</h1> <h2>${date.getSeconds()}</h2>`;
 console.log(date);
+
+let text = document.createTextNode(`Esto es un nodo`);
+document.getElementById("pp").appendChild(text);
+
+let clase = window.document.getElementById('id');
+console.log(clase);
+//clase.style.backgroundColor = '#ff9e01';
+clase.classList.add('negativo');
+
+let remove = document.getElementById('h');
+//h.classList.remove( 'clase');
+remove.style.cssText = "font-size: 40px; color: blue;";
+
+
+
+
+
+
+
+
+
