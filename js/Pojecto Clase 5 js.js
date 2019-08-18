42f44e14b9a530275529c887b47e4289a10be02f
Jorge Amador <rugama01@hotmail.com>
2019-08-18 14:14:54 -0600

    test

---
+++ js/Pojecto Clase 5 js.js
@@ -0,0 +1,9 @@
+let nombre = "El Mero Mero ElmerOmero";
+console.log(nombre);
+const numero = 8.14;
+
+// let d = document.getElementById("unico");
+let d = document.getElementsByClassName("clase");
+console.log(d);
+
+
