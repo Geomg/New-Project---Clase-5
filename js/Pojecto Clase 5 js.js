7c7aa412f7ab8db06b745613029d92f16ad480bc
Jorge Amador <rugama01@hotmail.com>
2019-08-18 14:59:02 -0600

    asdf

--- js/Pojecto Clase 5 js.js
+++ js/Pojecto Clase 5 js.js
@@ -5,5 +5,14 @@ const numero = 8.14;
 // let d = document.getElementById("unico");
 let d = document.getElementsByClassName("clase");
 console.log(d);
+console.log(d[0]);
+console.log(d.length);
+
+let date = new Date();
+
+document.body.innerHTML = `<h2>dehumo</h2>`;
+document.body.innerHTML = `<h2>${date.getHours()}</h2>`;
+document.body.innerHTML = `<h2>${date.getHours()}</h2> <h1>${date.getMinutes()}</h1> <h2>${date.getSeconds()}</h2>`;
 
 
+console.log(date);
